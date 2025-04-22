bda6889f655fc55238b36d0b8ccb585ee6b3aed9
saglamcanhasan <saglamcanhasan@gmail.com>
2025-04-22 16:49:15 +0300

    router update

--- src/router/router.jsx
+++ src/router/router.jsx
@@ -6,22 +6,6 @@ import ProductDetail from "../pages/product/product_detail.jsx";
 import LoginPage from "../pages/auth/login.jsx";
 import RegisterPage from "../pages/auth/register.jsx";
 import Cart from "../pages/cart/cart_page.jsx";
-<<<<<<< HEAD
-
-const AppRouter = () => {
-  return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Homepage />} />
-        <Route path="/product/:id" element={<ProductDetail />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/register" element={<RegisterPage />} />
-        <Route path="/cart" element={<Cart />} />
-        <Route path="/order-history" element={<OrderHistoryPage />} /> {/* New route for Order History */}
-        <Route path="/order/:orderID" element={<OrderDetailPage />} /> {/* New route for Order Detail */}
-      </Routes>
-    </Router>
-=======
 import { CartProvider } from "../pages/cart/cart_context";
 import Wishlist from "../pages/wishlist/wishlist.jsx";
 import ProfilePage from "../pages/profile/profilepage.jsx";
@@ -47,7 +31,6 @@ const AppRouter = () => {
         </Routes>
       </Router>
     </CartProvider>
->>>>>>> ede81827309b35c5130d236f8a120efbb0af026c
   );
 };
 
