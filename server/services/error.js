16d45a6af71b2457129562ee0c3b6214bdf9e02e
saglamcanhasan <saglamcanhasan@gmail.com>
2025-04-30 14:55:57 +0300

    server optimization

---
+++ server/services/error.js
@@ -0,0 +1,35 @@
+const createError = (message, status) => {
+    const error = new Error(message);
+    error.status = status;
+    return error;
+}
+
+
+const extractError = (error) => {
+    console.error(error);
+        
+    //extract error message and return 
+    let message = "Internal server error", status = 500;
+
+    switch (error.code) {
+        case "auth/id-token-expired":
+            message = "Invalid or expired token"; status = 401;
+            break;
+        case "auth/email-already-exists":
+            message = "This email is already registered"; status = 400;
+            break;
+        case "auth/invalid-email":
+            message = "Please enter a valid email address"; status = 400;
+            break;
+        case "auth/weak-password":
+            message = "Password should be at least 6 characters"; status = 400;
+            break;
+        default:
+            message = error.message; status = error.status
+            break;
+    }
+
+    return { message, status };
+}
+
+export default { createError, extractError };
---
+++ server/services/token.js
@@ -0,0 +1,21 @@
+import createError from "./error";
+
+
+const decodeToken = async (admin, database, token, condition) => {    
+    if (!token)
+        throw createError("No token provided", 401);
+
+    const decodedToken = await admin.auth().verifyIdToken(token);
+    const userReference = database.collection("users").doc(decodedToken.uid);
+    const userDocument = await userReference.get();
+    const userData = userDocument.data();
+    const tokenRole = userData.role;
+    const isUser = userDocument.id === decodedToken.uid;
+
+    if (!(user.exists) || !(condition(decodedToken, tokenRole, isUser, userData)))
+        throw createError("Unauthorized access", 401);
+
+    return { decodeToken, tokenRole, isUser };
+}
+
+export default decodeToken;
